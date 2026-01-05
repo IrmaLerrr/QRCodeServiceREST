df6dfaa419c2e02bcd129829aadb2bae2066ee66
IrinaLazarenko <irma.lerrr.wine@gmail.com>
2026-01-05 14:25:48 +0300

    add qrcode correctionLevels (stage 5)

--- src/main/java/com/example/demo/QRCodeController.java
+++ src/main/java/com/example/demo/QRCodeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 
 @RestController
@@ -19,14 +20,23 @@ private static final QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<BufferedImage> getImage(@RequestParam int size, @RequestParam String type, @RequestParam String contents) {
+    public ResponseEntity<BufferedImage> getImage(
+            @RequestParam(defaultValue = "250") int size,
+            @RequestParam(defaultValue = "png") String type,
+            @RequestParam(defaultValue = "L") String correction,
+            @RequestParam String contents) {
 
-        if (contents == null || contents.isBlank()) {
+        if (contents.isBlank()) {
             throw new IllegalArgumentException("Contents cannot be null or blank");
         }
 
         if (size < 150 || size > 350) {
             throw new IllegalArgumentException("Image size must be between 150 and 350 pixels");
+        }
+
+        correction = correction.toUpperCase();
+        if (!List.of("L", "M", "Q", "H").contains(correction)) {
+            throw new IllegalArgumentException("Contents cannot be null or blank");
         }
 
         MediaType mediaType = switch (type.toLowerCase()) {
@@ -36,7 +46,7 @@ private static final QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
             default -> throw new IllegalArgumentException("Only png, jpeg and gif image types are supported");
         };
 
-        BufferedImage bufferedImage = qrCodeGenerator.getQR(size, contents);
+        BufferedImage bufferedImage = qrCodeGenerator.getQR(size, contents, correction);
         return ResponseEntity
                 .ok()
                 .contentType(mediaType)
--- src/main/java/com/example/demo/QRCodeGenerator.java
+++ src/main/java/com/example/demo/QRCodeGenerator.java
@@ -1,20 +1,33 @@
 package com.example.demo;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.awt.image.BufferedImage;
+import java.util.Map;
 
 public class QRCodeGenerator {
 
-    public BufferedImage getQR(int size, String data) {
+    public BufferedImage getQR(int size, String data, String correction) {
         QRCodeWriter writer = new QRCodeWriter();
         BufferedImage bufferedImage = null;
+
+        ErrorCorrectionLevel correctionLevel = switch (correction.toUpperCase()) {
+            case "L" -> ErrorCorrectionLevel.L;
+            case "M" -> ErrorCorrectionLevel.M;
+            case "Q" -> ErrorCorrectionLevel.Q;
+            case "H" -> ErrorCorrectionLevel.H;
+            default -> throw new IllegalArgumentException("Permitted error correction levels are L, M, Q, H");
+        };
+        Map<EncodeHintType, ?> hints = Map.of(EncodeHintType.ERROR_CORRECTION, correctionLevel);
+
         try {
-            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, size, size);
+            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, size, size, hints);
             bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
         } catch (WriterException e) {
             e.printStackTrace();
